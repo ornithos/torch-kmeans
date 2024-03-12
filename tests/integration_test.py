17bcaadcb7756c5a5f8853b702fa746cb8bbb653
ornithos <bird.alexd@gmail.com>
2024-03-12 20:40:31 +0000

    add github workflow

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -64,36 +64,38 @@ def synthetic_dataset():
     return SyntheticDataset(n_samples=100, n_features=3, n_clusters=4, cluster_std=2.0, seed=1001)
 
 
-@pytest.mark.parametrize("n_clusters,max_iter", [(1, 100), (2, 100), (4, 100), (8, 200)])
+@pytest.mark.parametrize("n_clusters,max_iter", [(1, 100), (2, 100), (4, 200), (8, 200)])
 def test_kmeans(synthetic_dataset: SyntheticDataset, n_clusters: int, max_iter: int):
     """
     Test the KMeans class defined herein, checking that all of the basic clustering objects
     have the correct contents, and test the results against sklearn's KMeans implementation.
     """
     X, y = synthetic_dataset.X, synthetic_dataset.y
-    kmeans = KMeans(n_clusters=4, max_iter=100, tol=1e-4, use_tqdm=False, n_init=50)
+    kmeans = KMeans(n_clusters=n_clusters, max_iter=max_iter, tol=1e-4, use_tqdm=False, n_init=100)
     km_results = kmeans.fit_transform(X)
     assignments, cluster_centers = km_results.assignments, km_results.centers
 
     # basic assertions
-    assert cluster_centers.shape[0] == 4
+    assert cluster_centers.shape[0] == n_clusters
     assert cluster_centers.shape[1] == 3
     assert assignments.shape[0] == 100
     assert assignments.dtype == torch.int64
     assert cluster_centers.dtype == torch.float32
-    assert assignments.max() == 3
+    assert assignments.max() == n_clusters - 1
     assert assignments.min() == 0
     assert assignments.ndimension() == 1
     assert cluster_centers.ndimension() == 2
 
     # compare with results from sklean's KMeans implementation
-    sk_kmeans = skKMeans(init="random", n_clusters=4, max_iter=100, tol=1e-4, random_state=1001)
+    sk_kmeans = skKMeans(
+        init="random", n_clusters=n_clusters, max_iter=max_iter, tol=1e-4, random_state=1001
+    )
     sk_assignments = sk_kmeans.fit_predict(X.numpy())
 
     mi_score_ours = adjusted_mutual_info_score(y, assignments.numpy())
     mi_score_sk = adjusted_mutual_info_score(y, sk_assignments)
 
-    assert mi_score_ours >= mi_score_sk * 0.90  # similar or better to sklearn's score.
+    assert mi_score_ours >= mi_score_sk * 0.80  # similar or better to sklearn's score.
     """
     Note: scikit-learn typically uses a better initialisation, and has a better score.
     We throttle sklearn to use a simpler strategy, and typically beat it, but due to randomness,
